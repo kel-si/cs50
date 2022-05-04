185732e3b71a346ec45bb3ef5bbc6017c31d471a
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-04 10:25:03 -0600

    print message

---
+++ w1/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, World!\n");
+}
+
+// `make hello` - to compile the file
+
+// `./hello` to execute and see output
+
+// if the code is changed, `make hello` again to recompile
