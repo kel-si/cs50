7fa96f8f114ec7ef82ea5e594d0003c625973fb5
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-05 21:47:28 -0600

    print with variables and input from console

--- w1/hello.c
+++ w1/hello.c
@@ -1,8 +1,16 @@
+#include <cs50.h>
+// library
+
 #include <stdio.h>
+//library that gives you access to print outs
+
 
 int main(void)
 {
   printf("Hello, World!\n");
+  string answer = get_string("What's your name? ");
+  printf("Hello, %s\n", answer);
+
 }
 
 // `make hello` - to compile the file
