fa84c19185163d3487c2ed5af401d065e0e398bd
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-12 14:20:41 -0600

    refactor to use const variable

--- w1/conditionals.c
+++ w1/conditionals.c
@@ -3,17 +3,20 @@
 
 int main(void)
 {
+  // in C, const variables are often uppercase
+  const int MYFAV = 9;
+
   int answer = get_int("What's your favorite number? ");
 
-  if (answer < 9 )
+  if (answer < MYFAV )
   {
     printf("Your favorite number is less than mine.\n");
   }
-  if (answer > 9 )
+  if (answer > MYFAV )
   {
     printf("Your favorite number is greater than mine.\n");
   }
-  if (answer == 9 )
+ if (answer == MYFAV)
   {
     printf("We have the same favorite number!!\n");
   }
