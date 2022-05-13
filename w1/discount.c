871fe7d520b3b800795a8651a0a9b8b57a703647
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-12 22:43:59 -0600

    add percent to discount as a parameter

--- w1/discount.c
+++ w1/discount.c
@@ -1,18 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float discount(float price);
+float discount(float price, int percentage);
 
 int main(void)
 {
   float regular = get_float("Regular Price: ");
-  float sale = discount(regular);
+  int percent_off = get_int("Percent Off: ");
+  float sale = discount(regular, percent_off);
   //2 decimal places
   printf("Sale Price: %.2f\n", sale);
 }
 
-//function called discount that returns a float and takes in a price
-float discount(float price)
+//function called discount that returns a float and takes in a price and discount
+float discount(float price, int percentage)
 {
-  return price * 0.85;
+  return price * (100 - percentage) / 100;
 }
