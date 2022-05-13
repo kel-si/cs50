e6f8067731a5e998ef6a6881b75cd0b0a7d350fb
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-12 22:33:34 -0600

    create function to calculate discount

--- w1/discount.c
+++ w1/discount.c
@@ -1,10 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float discount(float price);
+
 int main(void)
 {
   float regular = get_float("Regular Price: ");
-  float sale = regular * 0.85;
+  float sale = discount(regular);
   //2 decimal places
   printf("Sale Price: %.2f\n", sale);
+}
+
+//function called discount that returns a float and takes in a price
+float discount(float price)
+{
+  return price * 0.85;
 }
