caa7528ed6cd4a866c495fe930fafcaefaae8547
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-18 17:00:15 -0600

    use an array to store integers instead

--- w2/scores.c
+++ w2/scores.c
@@ -2,13 +2,24 @@
 
 int main(void)
 {
-  int score1 = 72;
-  int score2 = 73;
-  int score3 = 33;
+  // a better way to store these scores would be to use an array.
+
+  // int score1 = 72;
+  // int score2 = 73;
+  // int score3 = 33;
+
+  // declaring an array with 3 integers
+  int scores[3];
+
+  //could set integers like this (no need to declare type here):
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
 
   // get error: "format specifies type 'double' but the argument has type 'int' [-Wformat]"
   // need to convert to float - we can change 3 to 3.0
   // printf("Average: %f\n", (score1 + score2 + score3) / 3);
 
-  printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+  printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
