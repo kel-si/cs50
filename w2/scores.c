aa6c325859c6e51c69b2783a1f24649c15c316ca
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-18 20:06:29 -0600

    use a loop to get scores and assign to array

--- w2/scores.c
+++ w2/scores.c
@@ -12,15 +12,21 @@ int main(void)
   // declaring an array with 3 integers
   int scores[3];
 
+  // loop through to assign scores to the array
+  for (int i = 0; i < 3; i++)
+  {
+    scores[i] = get_int("SCORE: ");
+  }
+
   //could set integers like this (no need to declare type here):
     // scores[0] = (73);
     // scores[1] = (72);
     // scores[2] = (33);
     
   // user input to get the scores
-    scores[0] = get_int("Score 1: ");
-    scores[1] = get_int("Score 2: ");
-    scores[2] = get_int("Score 3: ");
+    // scores[0] = get_int("Score 1: ");
+    // scores[1] = get_int("Score 2: ");
+    // scores[2] = get_int("Score 3: ");
 
   // get error: "format specifies type 'double' but the argument has type 'int' [-Wformat]"
   // need to convert to float - we can change 3 to 3.0
