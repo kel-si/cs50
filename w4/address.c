112391a5423aaa435c8db6caa289e8b97dc7bdb5
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-01 20:54:16 -0600

    memory in C and pointers

---
+++ w4/address.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* 
+Memory is made up of bytes, which is a bunch of 0s and 1s. A 'pointer' is a variable which stores the address of some variable - the location of some variable - or more specifically, a pointer stores the specific byte in which that variable is stored.
+
+The & operator in front of a variable name specifies you want the location of that variable
+
+To store that location you need to specify the type with: int *p
+
+This returns a hexadecimal location (ex: 0x16aec72b)
+
+In programming, to avoid confusion with which number system is being used (ex: decimal or hexadecimal), 0x is placed in front of the hexadecimal number
+
+*/
+
+int main(void)
+{
+    int n = 50;
+    int *p = &n;
+    printf("%p\n", p);
+}
