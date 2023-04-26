a22c06dcae51182431cdfb3edb0d0265d89bbc97
Kelsi Olstad <kelsiolstad@gmail.com>
2023-04-25 22:28:23 -0600

    Add more comments (refresh memory😄)

--- w4/address.c
+++ w4/address.c
@@ -14,14 +14,20 @@ In programming, to avoid confusion with which number system is being used (ex: d
 
 int main(void)
 {
+    // integer called n stored in memory somewhere (4 bytes) for example, 0x123
     int n = 50;
+    printf("%i\n", n); // 50
+
+    // print address of variable (& to not print value)
+    printf("%p\n", &n); // 0x16d05701c
+
+    // store address of n in a variable
     int *p = &n;
 
     // This will print a new location each time the file is ran
-
     // When printing the address, you do not include the asterisk - only when initially declaring
-    printf("%p\n", p);
+    printf("%p\n", p); // 0x16d05701c
 
     // If you wanted to print out the integer via %i at that address, include the asterisk (dereference the pointer)
-    printf("%i\n", *p);
+    printf("%i\n", *p); // 50
 }
